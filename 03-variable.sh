996a41dbcaaf81b65dd9cd8d2cea3478cf47b34f
rajve <rajvel@gmail.com>
2024-05-03 20:38:36 -0400

    exit status in frontend

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,5 +1,11 @@
-# define variable
+# define user-defined variable
 X=10
 
 # to access $var
 echo x is ${X}
+
+# system special variable
+# $0, $1, $?, $n
+# $#, $*
+# exit status - 0-255
+# 0 -success; 1-255 failure foro their own commands
