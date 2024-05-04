582aa9e5a12562e29b55efca9f13967b99353e67
rajve <rajvel@gmail.com>
2024-05-03 20:07:25 -0400

    print colors demo

---
+++ 02-print.sh
@@ -0,0 +1,11 @@
+# syntax : echo -e  "\e[colourmessage\e]0m"
+# echo -e , -e is to enable colors
+# \e[COLm - COL is color code -Red(31), Green(32), yellow(33), blue(34), magenta(35), cyan(36)
+# \e[0m - Disable the enabled color
+
+echo -e "\e[31mHave a nice day\e[0m"
+echo -e "\e[32mHave a nice day\e[0m"
+echo -e "\e[33mHave a nice day\e[0m"
+echo -e "\e[34mHave a nice day\e[0m"
+echo -e "\e[35mHave a nice day\e[0m"
+echo -e "\e[36mHave a nice day\e[0m"
