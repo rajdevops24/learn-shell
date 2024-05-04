33034332859ff8712bf21b572aee2ff76c2adba9
rajve <rajvel@gmail.com>
2024-05-03 23:15:02 -0400

    function example

--- 04-redirectors.sh
+++ 04-redirectors.sh
@@ -3,7 +3,15 @@ print_hello() {
    echo Have a nice day
    y=20
 }
+
+inputs_function() {
+   echo First arg - $1
+  echo second arg - $2
+  echo All arg - $*
+}
 x=20
 print_hello
 
 echo y from function - $y
+
+inputs_function jkl 789 asd wer
